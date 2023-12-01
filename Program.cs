ff4f9ebe8d050979a5ea6d0c225859508f3d816b
Demetrius Wingate <dwingate1991@gmail.com>
2023-12-01 18:40:42 -0500

    added switch statements

--- Program.cs
+++ Program.cs
@@ -1,33 +1,158 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using InventoryList.Logic;
-using InventoryList.Data;
-using System.Text.Json;
-using System.Text.RegularExpressions;
-using System.Globalization;
-using InventoryList.UI;
+﻿using System;
+using System.Collections.Generic;
 
-internal class Program
+class InventoryItem
 {
-    private static void Main(string[] args)
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public double Price { get; set; }
+}
+
+class InventoryManagementSystem
+{
+    private List<InventoryItem> inventory;
+
+    public InventoryManagementSystem()
     {
-        var services = CreateServiceCollection();
-
-        var groceryLogic = services.GetService<IGroceryLogic>();
-
-        UserInterface.WelcomeBanner();
-
-        UserInterface.MainMenu(groceryLogic);
+        inventory = new List<InventoryItem>();
     }
 
-    static IServiceProvider CreateServiceCollection()
+    public void AddItem(string name, int quantity, double price)
     {
-        return new ServiceCollection()
-            .AddTransient<IGroceryLogic, GroceryLogic>()
-            .AddTransient<IGroceryItemRepository, GroceryItemRepository>()
-            .BuildServiceProvider();
+        InventoryItem newItem = new InventoryItem
+        {
+            Name = name,
+            Quantity = quantity,
+            Price = price
+        };
+
+        inventory.Add(newItem);
+        Console.WriteLine("Item added to inventory.");
     }
 
+    public void UpdateItem(string name, int newQuantity, double newPrice)
+    {
+        InventoryItem itemToUpdate = inventory.Find(item => item.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+        if (itemToUpdate != null)
+        {
+            itemToUpdate.Quantity = newQuantity;
+            itemToUpdate.Price = newPrice;
+            Console.WriteLine($"Item '{name}' updated in inventory.");
+        }
+        else
+        {
+            Console.WriteLine($"Item '{name}' not found in inventory. Update failed.");
+        }
+    }
+
+    public void DeleteItem(string name)
+    {
+        InventoryItem itemToRemove = inventory.Find(item => item.Name.Equals(name, StringComparison.OrdinalIgnoreCase));
+
+        if (itemToRemove != null)
+        {
+            inventory.Remove(itemToRemove);
+            Console.WriteLine($"Item '{name}' deleted from inventory.");
+        }
+        else
+        {
+            Console.WriteLine($"Item '{name}' not found in inventory.");
+        }
+    }
+
+    public void DisplayInventory()
+    {
+        Console.WriteLine("Inventory:");
+
+        foreach (var item in inventory)
+        {
+            Console.WriteLine($"Name: {item.Name}, Quantity: {item.Quantity}, Price: {item.Price}");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        InventoryManagementSystem ims = new InventoryManagementSystem();
+
+        ims.AddItem("Anesthesia Machine", 12, 2750.25);
+        ims.AddItem("Oxygen Concentrator", 5, 1400.25);
+
+        ims.DisplayInventory();
+
+        // Add more interactions as needed
+        Console.WriteLine("\nChoose an option:");
+        Console.WriteLine("1. Add Item");
+        Console.WriteLine("2. Update Item");
+        Console.WriteLine("3. Delete Item");
+        Console.WriteLine("4. Display Inventory");
+        Console.WriteLine("0. Exit");
+
+        int choice;
+        while (true)
+        {
+            Console.Write("Enter your choice: ");
+            if (int.TryParse(Console.ReadLine(), out choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("\nEnter new item details:");
+                        Console.Write("Enter item name: ");
+                        string newItemName = Console.ReadLine();
+
+                        Console.Write("Enter quantity: ");
+                        int newItemQuantity = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter price: ");
+                        double newItemPrice = double.Parse(Console.ReadLine());
+
+                        ims.AddItem(newItemName, newItemQuantity, newItemPrice);
+                        ims.DisplayInventory();
+                        break;
+
+                    case 2:
+                        Console.Write("Enter the name of the item to update: ");
+                        string updateItemName = Console.ReadLine();
+
+                        Console.Write("Enter new quantity: ");
+                        int updateItemQuantity = int.Parse(Console.ReadLine());
+
+                        Console.Write("Enter new price: ");
+                        double updateItemPrice = double.Parse(Console.ReadLine());
+
+                        ims.UpdateItem(updateItemName, updateItemQuantity, updateItemPrice);
+                        ims.DisplayInventory();
+                        break;
+
+                    case 3:
+                        Console.Write("Enter the name of the item to delete: ");
+                        string deleteItemName = Console.ReadLine();
+                        ims.DeleteItem(deleteItemName);
+                        break;
+
+                    case 4:
+                        ims.DisplayInventory();
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Exiting the program.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please enter a valid option.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+    }
 }
 
 
